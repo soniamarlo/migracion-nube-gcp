23b70815c86efa80223f97749fbd797435ffe515
soniamarlo <63555832+soniamarlo@users.noreply.github.com>
2024-07-16 20:41:12 +0200

    Update script-atacarIpAutoescalado.py

--- script-atacarIpAutoescalado.py
+++ script-atacarIpAutoescalado.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 if len(sys.argv) != 2:
     print("Usage: python attack_script.py <target_ip>")
@@ -9,11 +10,21 @@ if len(sys.argv) != 2:
 target_ip = sys.argv[1]
 url = f"http://{target_ip}"
 
-while True:
+def make_request(url):
     try:
         response = requests.get(url)
         print(f"Requested {url} - Status Code: {response.status_code}")
     except requests.exceptions.RequestException as e:
         print(f"Error requesting {url}: {e}")
-    time.sleep(2)
 
+# Número de solicitudes concurrentes
+num_requests = 10  # Puedes ajustar este número según sea necesario
+
+# Utilizando ThreadPoolExecutor para realizar múltiples solicitudes en paralelo
+with ThreadPoolExecutor(max_workers=num_requests) as executor:
+    for _ in range(num_requests):
+        executor.submit(make_request, url)
+        time.sleep(0.2)  # Añadir un pequeño retraso entre las solicitudes
+
+# Esperar un momento para permitir que se completen todas las solicitudes
+time.sleep(5)  # Ajusta el tiempo según sea necesario
