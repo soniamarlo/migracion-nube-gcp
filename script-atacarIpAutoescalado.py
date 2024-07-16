e1c640e249a951b8da6a716ba72f3dab02d56460
soniamarlo <63555832+soniamarlo@users.noreply.github.com>
2024-07-16 19:45:01 +0200

    Update script-atacarIpAutoescalado.py

--- script-atacarIpAutoescalado.py
+++ script-atacarIpAutoescalado.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import requests
 
 if len(sys.argv) != 2:
@@ -8,18 +9,11 @@ if len(sys.argv) != 2:
 target_ip = sys.argv[1]
 url = f"http://{target_ip}"
 
-# Definir el número de veces que se ejecutará el ataque
-num_requests = 100
-
-for i in range(num_requests):
+while True:
     try:
         response = requests.get(url)
         print(f"Requested {url} - Status Code: {response.status_code}")
     except requests.exceptions.RequestException as e:
         print(f"Error requesting {url}: {e}")
-
-    # No se necesita tiempo de espera si queremos ejecutar las solicitudes lo más rápido posible
-    # En un escenario real, podrías considerar agregar una pausa para no sobrecargar el servidor objetivo
-
-print("Attack completed.")
+    time.sleep(2)
 
